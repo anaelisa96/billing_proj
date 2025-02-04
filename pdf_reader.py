0340b8eabeb893ca0fb278d6b6c13a84d0cae065
Ana Elisa <anaelisa96@live.com.pt>
2025-02-04 22:25:47 +0000

    Separate text parsinf and exporting, improving code readability

--- pdf_reader.py
+++ pdf_reader.py
@@ -11,55 +11,58 @@ def get_text_from_pdf(filename):
         # Get pdf 1st page (assuming there is only one)
         return pdf[0]
 
-def text_parser(text, filename):
+def text_parser(text):
     # Build list with each text line
     comp_list = text.split('\n')
     # Remove empty elements from list
     comp_list = list(filter(None, comp_list))
-    # Get invoice number and date
-    inv_nb = comp_list[comp_list.index("INVOICE #") + 2]
-    date = comp_list[comp_list.index("DATE") + 2]
-    print("inv. number " + inv_nb + " date " + date)
-    # Get start and end index to get items data
+    # Get invoice number and date headers indexes
+    inv_nb_hdr_idx = comp_list.index("INVOICE #")
+    date_hdr_idx = comp_list.index("DATE")
+    # Get start and end index to get data
     idx = comp_list.index("ITEMS")
     end_idx = comp_list.index("NOTES:")
-    row = 0
-    wb, ws = open_excel(filename)
+    # Initialize empty list of items lists
+    items_list = []
+    # First iteration of the loop woll be the header titles
+    is_header = True
     while idx < end_idx:
+        if is_header == True:
+            inv = comp_list[inv_nb_hdr_idx]
+            date = comp_list[date_hdr_idx]
+            is_header = False
+        else:
+            inv = comp_list[inv_nb_hdr_idx + 2]
+            date = comp_list[date_hdr_idx + 2]
         name   = comp_list[idx]
         desc   = comp_list[idx + 1]
         qty    = comp_list[idx + 2]
         price  = comp_list[idx + 3]
         tax    = comp_list[idx + 4]
         amount = comp_list[idx + 5]
-        item_list = []
-        item_list.extend([name, desc, qty, price, tax, amount])
-        write_to_excel(ws, item_list, row)
-        print("name " + name + " desc " + desc + " qty " + qty + " price " + price + " tax " + tax + " amount " + amount)
+        items_list.append([inv, date, name, desc, qty, price, tax, amount])
         idx += 6
-        row += 1
-    close_excel(wb)
+    return items_list
 
-def open_excel(filename):
-    workbook = xlsxwriter.Workbook(filename)
+def export_data(excel_filename, items_list):
+    workbook = xlsxwriter.Workbook(excel_filename)
     worksheet = workbook.add_worksheet()
-    return workbook, worksheet
-
-def close_excel(workbook):
-    workbook.close()
-
-def write_to_excel(worksheet, item_list, row):
-    column = 0
-    for elem in item_list:
-        worksheet.write(row, column, elem)
-        column += 1
+    row = 0
+    for list in items_list:
+        column = 0
+        for elem in list:
+            worksheet.write(row, column, elem)
+            column += 1
+        row += 1
 
 # Defining main function
 def main():
     # Retrieve the text from the 1st PDF page
     text = get_text_from_pdf('Invoice.pdf')
     # Parsing the data
-    text_parser(text, 'InvoiceData.xlsx')
+    items = text_parser(text)
+    # Export data to excel
+    export_data('InvoiceData.xlsx', items)
 
 if __name__=="__main__":
     main()
